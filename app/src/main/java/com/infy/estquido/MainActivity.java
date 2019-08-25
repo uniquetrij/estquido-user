dae5e5e14bee7e3cb3b7b8bb1bd6f28385aeeb65
anupam <anupamb266@gmail.com>
2019-08-25 17:09:49 +0530

    runtime location permissions added

--- app/src/main/java/com/infy/estquido/MainActivity.java
+++ app/src/main/java/com/infy/estquido/MainActivity.java
@@ -1,28 +1,66 @@
 package com.infy.estquido;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.infy.estquido.app.This;
 import com.infy.estquido.app.services.CouchBaseService;
 
-import java.sql.Time;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 
+public class MainActivity extends AppCompatActivity  implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = MainActivity.class.getName();
+    private static final int REQUEST_CHECK_SETTINGS = 200;
     private FusedLocationProviderClient mFusedLocationClient;
+    private GoogleApiClient mGoogleApiClient;
+
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 555;
+    int ACCESS_FINE_LOCATION_CODE = 3310;
+    int ACCESS_COARSE_LOCATION_CODE = 3410;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +71,162 @@ public class MainActivity extends AppCompatActivity {
         This.APPLICATION.set(getApplication());
         This.MAIN_ACTIVITY.set(this);
 
+        // Build Google API Client for Location related work
+        buildGoogleApiClient();
+
+    }
+    // When user first come to this activity we try to connect Google services for location and map related work
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    // Google Api Client is connected
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (mGoogleApiClient.isConnected()) {
+            //if connected successfully show user the settings dialog to enable location from settings services
+            // If location services are enabled then get Location directly
+            // Else show options for enable or disable location services
+            settingsrequest();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        settingsrequest();
+    }
+
+    public void settingsrequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true); //this is the key ingredient
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        if (mGoogleApiClient.isConnected()) {
+
+                            // check if the device has OS Marshmellow or greater than
+                            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                                if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_CODE);
+                                } else {
+                                    // get Location
+                                }
+                            } else {
+                                // get Location
+                            }
+
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(MainActivity.this, REQUEST_RESOLVE_ERROR);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+    }
+
+    // This method is called only on devices having installed Android version >= M (Marshmellow)
+    // This method is just to show the user options for allow or deny location services at runtime
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 3310: {
+
+                if (grantResults.length > 0) {
+
+                    for (int i = 0, len = permissions.length; i < len; i++) {
+
+                        if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                            // Show the user a dialog why you need location
+                        } else if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                            // get Location
+                        } else {
+                            this.finish();
+                        }
+                    }
+                }
+                return;
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    inferCenter();
+                    break;
+                case Activity.RESULT_CANCELED:
+                    this.finish();
+                    break;
+            }
+        }
+    }
+    // Connect Google Api Client if it is not connected already
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    // Stop the service when we are leaving this activity
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void inferCenter() {
+        Toast.makeText(getApplicationContext(),"infer center reached",Toast.LENGTH_LONG).show();
+
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
+
                 if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    Activity#requestPermissions
--- app/src/main/java/com/infy/estquido/app/This.java
+++ app/src/main/java/com/infy/estquido/app/This.java
@@ -1,7 +1,12 @@
 package com.infy.estquido.app;
 
+import android.Manifest;
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.infy.estquido.MainActivity;
 
@@ -14,11 +19,12 @@ public class This {
     public static final AtomicReference<Application> APPLICATION = new AtomicReference<>();
     public static final AtomicReference<MainActivity> MAIN_ACTIVITY = new AtomicReference<>();
 
-
     public static class Static {
         public static final String COUCHBASE_URL = "ws://192.168.1.101:4984/estquido";
         public static final String COUCHBASE_DB = "estquido";
         public static final String COUCHBASE_USER = "estquido";
         public static final String COUCHBASE_PASS = "estquido";
     }
+
+
 }
